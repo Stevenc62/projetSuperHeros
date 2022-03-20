11125370d98616ee13e2c940c50461f1a05dfdc3
Stevenc62 <steven.caron@viacesi.fr>
2022-03-20 17:19:27 +0100

    Fix

--- src/main/java/com/steven/projetSuperHeros/ServletInitializer.java
+++ src/main/java/com/steven/projetSuperHeros/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.caisseenregistreusemvc;
+package com.steven.projetSuperHeros;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(CaisseEnregistreuseMvcApplication.class);
+        return application.sources(ProjetSuperHerosApplication.class);
     }
 
 }
